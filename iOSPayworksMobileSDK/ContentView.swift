7255bc5bac3ef2fb16020fb523cecad2db01e77c
Christopher Stock <christopher.stock@mayflower.de>
2021-09-01 08:48:50 +0200

    Empty iOS template app

---
+++ iOSPayworksMobileSDK/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSPayworksMobileSDK
+//
+//  Created by Christopher Stock on 31.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
